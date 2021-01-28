dd07d63876f263dc1cbd3205e66f0372414e234f
wangh <123456>
2021-01-28 15:37:33 +0800

    添加算法

---
+++ app/src/main/java/com/ssdk/zxt/ZxtUtils.kt
@@ -0,0 +1,14 @@
+package com.ssdk.zxt
+
+/**
+ *Copyright:Copyright@2019成都捷德科技有限公司版权
+ *
+ * @Author
+ * @Date 2021/1/28
+ * @Version 2.0
+ */
+class ZxtUtils {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
