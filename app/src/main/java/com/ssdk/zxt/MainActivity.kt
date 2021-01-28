5b34081cbd3cab1c537deaf055d5bd0ea2bb88c0
wangh <123456>
2021-01-28 15:02:05 +0800

    Initial commit

---
+++ app/src/main/java/com/ssdk/zxt/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ssdk.zxt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.ssdk.mylibrary.ZXT
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ZXT.write()
+    }
+}
---
+++ mylibrary/src/main/java/com/ssdk/mylibrary/ZXT.kt
@@ -0,0 +1,13 @@
+package com.ssdk.mylibrary
+
+import android.util.Log
+
+
+class ZXT {
+    companion object{
+        fun write() {
+            Log.e("zxt", "我是 library 数据")
+        }
+    }
+
+}
