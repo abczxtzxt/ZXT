a29dc6e2dcfe36c1928fbe8039876ab01872b147
wangh <123456>
2021-01-29 14:02:21 +0800

    添加网络库

---
+++ zxt-network/src/main/java/com/ssdk/zxt_network/NetworkHelper.kt
@@ -0,0 +1,20 @@
+package com.ssdk.zxt_network
+
+import android.util.Log
+
+/**
+ *Copyright:Copyright@2019成都捷德科技有限公司版权
+ *
+ * @Author
+ * @Date 2021/1/29
+ * @Version 2.0
+ */
+class NetworkHelper {
+
+    companion object {
+
+        fun post() {
+            Log.e("network", "我是post方法")
+        }
+    }
+}
